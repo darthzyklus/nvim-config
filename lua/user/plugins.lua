23e59f744f653dc722c85bc080735bb5c3772e85
Andres Uris <andres.urismontilla@gmail.com>
2022-11-05 10:59:09 -0400

    add harpoon plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -54,6 +54,9 @@ return packer.startup(function(use)
   -- Telescope
   use "nvim-telescope/telescope.nvim"
 
+	-- Harpoon
+	use 'ThePrimeagen/harpoon'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
